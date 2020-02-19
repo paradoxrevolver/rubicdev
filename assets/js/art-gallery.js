cfd79531726edbf240a7e693785016314e1d531a
rubic <adamtheball@gmail.com>
2020-02-19 14:13:20 -0800

    started new galleries
    switched to visual studio code

--- assets/js/art-gallery.js
+++ assets/js/art-gallery.js
@@ -1,4 +1,13 @@
-$(document).ready(() => {
-  let $frames = $('#art-gallery .frame');
-  console.log($frames);
+var $artGallery = $('#art-gallery');
+var $frames = $artGallery.children('.frame');
+
+[...$frames].forEach((frame) => {
+  var $frame = $(frame);
+  $frame.on('click', () => {
+    if($frame.hasClass('focus')) $frame.removeClass('focus');
+    else {
+      $frames.removeClass('focus');
+      $frame.addClass('focus');
+    }
+  });
 });
