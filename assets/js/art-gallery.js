34fdb865cd5db87da6e9efd1c072969324cb5eab
paradoxrevolver <adamtheball@gmail.com>
2021-04-03 16:20:55 -0700

    new art gallery for graphic design

--- assets/js/art-gallery.js
+++ assets/js/art-gallery.js
@@ -1,12 +1,13 @@
-var $artGallery = $('#art-gallery');
-var $frames = $artGallery.children('.frame');
+var $allFrames = $('.art-gallery .frame');
 
-[...$frames].forEach((frame) => {
-  var $frame = $(frame);
+$allFrames.each((i, el) => {
+  var $frame = $(el);
   $frame.on('click', () => {
-    if($frame.hasClass('focus')) $frame.removeClass('focus');
-    else {
-      $frames.removeClass('focus');
+    if($frame.hasClass('focus')) {
+      $frame.removeClass('focus');
+    } else {
+      // remove focus class only from frames in THIS art gallery
+      $frame.closest('.art-gallery').children('.frame').removeClass('focus');
       $frame.addClass('focus');
     }
   });
