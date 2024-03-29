12c475f2ab4be64c3c0fe482dfe663611a3b123b
rubic <adamtheball@gmail.com>
2024-03-28 20:55:38 -0700

    Catching up on a large number of updates made over the last couple of years
    - Starwind Shrine was created and pawb.in was moved off of the gamedev section
    - reset.css was "updated," which involved me doing an update a year ago and then deciding to undo it today, after some consideration
    - Several files had their formatting updated to use 4-space-width tabbing
    - p5.js was added to the project as a minified file, and the starfield project was started once again

--- assets/js/art-gallery.js
+++ assets/js/art-gallery.js
@@ -1,16 +1,19 @@
 $(function () {
-  var $allFrames = $('.art-gallery .frame');
+    var $allFrames = $(".art-gallery .frame");
 
-  $allFrames.each((i, el) => {
-    var $frame = $(el);
-    $frame.on('click', () => {
-      if($frame.hasClass('focus')) {
-        $frame.removeClass('focus');
-      } else {
-        // remove focus class only from frames in THIS art gallery
-        $frame.closest('.art-gallery').children('.frame').removeClass('focus');
-        $frame.addClass('focus');
-      }
+    $allFrames.each((i, el) => {
+        var $frame = $(el);
+        $frame.on("click", () => {
+            if ($frame.hasClass("focus")) {
+                $frame.removeClass("focus");
+            } else {
+                // remove focus class only from frames in THIS art gallery
+                $frame
+                    .closest(".art-gallery")
+                    .children(".frame")
+                    .removeClass("focus");
+                $frame.addClass("focus");
+            }
+        });
     });
-  });
-})
+});
--- assets/js/starfield.js
+++ assets/js/starfield.js
@@ -1,41 +1,62 @@
 /*
 	Created by rubic aka paradoxrevolver
-	Last updated 19.06.28
+	Last updated 24.03.28
 
 	This function represents a single instance and places on a canvas element given to it.
 	It generates a background of interactable drifting pixelated stars.
 */
-let starfield = function(canvas) {
-  if(!(canvas instanceof HTMLElement))
-    throw "starfield instance did not receive an HTMLElement";
 
-  let _ = this;
-  let starfieldCanv = canvas;
-  let s = canvas.getContext('2d', {
-    alpha: true
-  });
+// let starfield = function (canvas) {
+//     if (!(canvas instanceof HTMLElement))
+//         throw "starfield instance did not receive an HTMLElement";
 
-  _.Update = function() {
+//     let _ = this;
+//     let starfieldCanv = canvas;
+//     let s = canvas.getContext("2d", {
+//         alpha: true,
+//     });
 
-  }
+//     _.Update = function () {};
 
-  // run once for every animation frame
-  _.Step = function() {
-    // update values
-    _.Update();
-    // get a new animation frame
-    window.requestAnimationFrame(_.Step);
-  }
+//     // run once for every animation frame
+//     _.Step = function () {
+//         // update values
+//         _.Update();
+//         // get a new animation frame
+//         window.requestAnimationFrame(_.Step);
+//     };
 
-  _.Step();
+//     _.Step();
+// };
+
+// // inner scope so currentScript is scoped to this script
+// {
+//     let currentScript = document.currentScript;
+//     // runs when page is ready
+//     $(() => {
+//         let canvas = $(currentScript).siblings("canvas.starfield-canvas")[0];
+//         new starfield(canvas);
+//     });
+// }
+
+const starfield = (ctx) => {
+
+	let x = 100;
+	let y = 100;
+  
+	ctx.setup = () => {
+		ctx.createCanvas(200, 200);
+	}
+
+	ctx.draw = () => {
+		ctx.background(0);
+		ctx.fill(255);
+		ctx.rect(x,y,50,50);
+	}
 }
 
-// inner scope so currentScript is scoped to this script
-{
-  let currentScript = document.currentScript;
-  // runs when page is ready
-  $(() => {
-    let canvas = $(currentScript).siblings('canvas.starfield-canvas')[0];
-    new starfield(canvas);
-  });
-}
+let containers = $('.starfield')
+
+containers.each((_, el) => {
+	new p5(starfield, el);
+})
