1f741addf04dc0d3701ed023690b571605d84940
rubic <adamtheball@gmail.com>
2024-04-04 19:28:49 -0700

    Completed starfield
    Probably not the best performance, but this is a very good start.

--- assets/js/starfield.js
+++ assets/js/starfield.js
@@ -6,57 +6,187 @@
 	It generates a background of interactable drifting pixelated stars.
 */
 
-// let starfield = function (canvas) {
-//     if (!(canvas instanceof HTMLElement))
-//         throw "starfield instance did not receive an HTMLElement";
-
-//     let _ = this;
-//     let starfieldCanv = canvas;
-//     let s = canvas.getContext("2d", {
-//         alpha: true,
-//     });
-
-//     _.Update = function () {};
-
-//     // run once for every animation frame
-//     _.Step = function () {
-//         // update values
-//         _.Update();
-//         // get a new animation frame
-//         window.requestAnimationFrame(_.Step);
-//     };
-
-//     _.Step();
-// };
-
-// // inner scope so currentScript is scoped to this script
-// {
-//     let currentScript = document.currentScript;
-//     // runs when page is ready
-//     $(() => {
-//         let canvas = $(currentScript).siblings("canvas.starfield-canvas")[0];
-//         new starfield(canvas);
-//     });
-// }
-
 const starfield = (ctx) => {
+    let canvas = null;
+    let canvasParent = null;
+    let bgStars = [];
+    let fgStars = [];
+    const BG_STAR_COUNT = 600;
+    const FG_STAR_COUNT = 50;
+    const MULT_DRIFT_SPEED = 0.6;
 
-	let x = 100;
-	let y = 100;
-  
-	ctx.setup = () => {
-		ctx.createCanvas(200, 200);
-	}
+    ctx.setup = () => {
+        canvas = ctx.createCanvas(200, 200);
+        canvasParent = canvas.parent();
 
-	ctx.draw = () => {
-		ctx.background(0);
-		ctx.fill(255);
-		ctx.rect(x,y,50,50);
-	}
-}
+        for (let i = 0; i < BG_STAR_COUNT; i++) {
+            let closeness = Math.random();
+            bgStars.push({
+                x: Math.random(),
+                y: Math.random(),
+                closeness: closeness,
+                innerRadius: 0.75 - closeness * 0.25,
+                outerRadius: 1.5 + closeness * 2.5,
+                numPoints: Math.floor(3 + Math.random() * 3),
+                driftSpeed: 0.3 + closeness * 0.7,
+                spinSpeed: 0.1 + Math.random() * 0.9,
+            });
+        }
 
-let containers = $('.starfield')
+        for (let i = 0; i < FG_STAR_COUNT; i++) {
+            fgStars.push({
+                x: Math.random(),
+                y: Math.random(),
+                innerRadius: 1.0,
+                outerRadius: 5.0 + Math.random() * 2.0,
+                numPoints: Math.floor(3 + Math.random() * 3),
+                driftSpeed: 1.0 + Math.random() * 0.3,
+                spinSpeed: 0.3 + Math.random() * 1.5,
+            });
+        }
+    };
 
-containers.each((_, el) => {
-	new p5(starfield, el);
-})
+    ctx.draw = () => {
+        const CW =
+            canvasParent.clientWidth <= ctx.windowWidth
+                ? canvasParent.clientWidth
+                : ctx.windowWidth;
+        const CH =
+            canvasParent.clientHeight <= ctx.windowHeight
+                ? canvasParent.clientHeight
+                : ctx.windowHeight;
+        // set canvas to parent size every frame
+        ctx.resizeCanvas(CW, CH);
+
+        ctx.background(0, 0);
+        ctx.stroke(0, 0);
+
+        bgStars.forEach((s) => {
+            ctx.push();
+            ctx.fill(255, 255 * s.closeness * s.closeness);
+
+            // stars drift
+            s.x += s.driftSpeed / 2000.0 * MULT_DRIFT_SPEED;
+            s.y += (s.driftSpeed / 2000.0 * MULT_DRIFT_SPEED) * 0.75;
+
+            // stars move away from mouse
+            const radius = 0.26;
+            const dx = ctx.mouseX / CW - s.x; // vector from star to mouse
+            const dy = ctx.mouseY / CH - s.y;
+            const dmag = Math.sqrt(dx * dx + dy * dy); // magnitude of distance
+            const dmagr = radius - dmag; // remaining magnitude to edge of radius
+
+            if (dmag <= radius) {
+                s.x -= dmagr * dmagr * dmagr * s.closeness * dx;
+                s.y -= dmagr * dmagr * dmagr * s.closeness * dy;
+            }
+
+            // stars loop across the canvas
+            s.x %= 1.0;
+            s.y %= 1.0;
+
+            // move stars
+            ctx.translate(s.x * CW, s.y * CH);
+
+            // spin stars
+            ctx.rotate(s.spinSpeed * (ctx.frameCount / 50.0));
+
+            // draw
+            drawStar(0, 0, s.innerRadius, s.outerRadius, s.numPoints);
+            ctx.pop();
+        });
+
+        fgStars.forEach((s) => {
+            ctx.push();
+            ctx.fill(255);
+
+            // stars drift
+            s.x += s.driftSpeed / 2000.0 * MULT_DRIFT_SPEED;
+            s.y += (s.driftSpeed / 2000.0 * MULT_DRIFT_SPEED) * 0.75;
+
+            // stars move away from mouse
+            const radius = 0.26;
+            const dx = ctx.mouseX / CW - s.x; // vector from star to mouse
+            const dy = ctx.mouseY / CH - s.y;
+            const dmag = Math.sqrt(dx * dx + dy * dy); // magnitude of distance
+            const dmagr = radius - dmag; // remaining magnitude to edge of radius
+
+            if (dmag <= radius) {
+                s.x -= dmagr * dmagr * dmagr * dx;
+                s.y -= dmagr * dmagr * dmagr * dy;
+            }
+
+            // stars loop across the canvas
+            s.x %= 1.0;
+            s.y %= 1.0;
+
+            // move stars
+            ctx.translate(s.x * CW, s.y * CH);
+
+            // spin stars
+            ctx.rotate(s.spinSpeed * (ctx.frameCount / 50.0));
+
+            // draw
+            drawStar(0, 0, s.innerRadius, s.outerRadius, s.numPoints);
+            ctx.pop();
+        });
+
+        // go through each star[i] and connect to each star[j]
+        for (let i = 0; i < fgStars.length; i++) {
+            for (let j = 0; j < fgStars.length; j++) {
+                if (i === j) continue; // skip connecting this i star to itself
+                if (j > i) continue; // skip connecting this j star back to an i star which is already connected
+
+                ctx.push();
+                ctx.fill(255);
+
+                const ax = fgStars[i].x;
+                const ay = fgStars[i].y;
+                const bx = fgStars[j].x;
+                const by = fgStars[j].y;
+
+                const dx = bx - ax;
+                const dy = by - ay;
+                const dist = Math.sqrt(dx * dx + dy * dy);
+
+                const dmx = ctx.mouseX / CW - ax; // vector from star A to mouse
+                const dmy = ctx.mouseY / CH - ay;
+                const mdist = Math.sqrt(dmx * dmx + dmy * dmy);
+
+                ctx.stroke(
+                    255,
+                    255 * Math.pow(1 - dist, 15) * Math.pow(1 - mdist, 6)
+                );
+
+                ctx.line(ax * CW, ay * CH, bx * CW, by * CH);
+                ctx.pop();
+            }
+        }
+    };
+
+    // copied from the p5.js reference documentation
+    function drawStar(x, y, radius1, radius2, npoints) {
+        const TWO_PI = Math.PI * 2;
+        let angle = ctx.TWO_PI / npoints;
+        let halfAngle = angle / 2.0;
+        ctx.beginShape();
+        for (let a = 0; a < TWO_PI; a += angle) {
+            let sx = x + Math.cos(a) * radius2;
+            let sy = y + Math.sin(a) * radius2;
+            ctx.vertex(sx, sy);
+            sx = x + Math.cos(a + halfAngle) * radius1;
+            sy = y + Math.sin(a + halfAngle) * radius1;
+            ctx.vertex(sx, sy);
+        }
+        ctx.endShape();
+    }
+};
+
+window.addEventListener("load", (_) => {
+    let containers = $(".starfield");
+    console.log(containers)
+
+    containers.each((_, el) => {
+        new p5(starfield, el);
+    });
+});
